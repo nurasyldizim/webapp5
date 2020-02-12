4f57f958a342c49f171f9b76919c9890dbdfc034
Nurasyl <n.dizimov@p-s.kz>
2020-02-12 17:14:12 +0600

    Initial commit!

---
+++ src/main/java/com/webapp5/webapp5/Webapp5Application.java
@@ -0,0 +1,13 @@
+package com.webapp5.webapp5;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Webapp5Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Webapp5Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/webapp5/webapp5/Webapp5ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.webapp5.webapp5;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Webapp5ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
