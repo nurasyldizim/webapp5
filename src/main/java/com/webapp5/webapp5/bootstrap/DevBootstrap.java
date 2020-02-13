8ff9739f428eed205c69619bf085a0df4865baa9
Nurasyl <n.dizimov@p-s.kz>
2020-02-13 17:12:38 +0600

    Publisher Correct Version

--- src/main/java/com/webapp5/webapp5/bootstrap/DevBootstrap.java
+++ src/main/java/com/webapp5/webapp5/bootstrap/DevBootstrap.java
@@ -2,8 +2,10 @@ package com.webapp5.webapp5.bootstrap;
 
 import com.webapp5.webapp5.model.Author;
 import com.webapp5.webapp5.model.Book;
+import com.webapp5.webapp5.model.Publisher;
 import com.webapp5.webapp5.repositories.AuthorRepository;
 import com.webapp5.webapp5.repositories.BookRepository;
+import com.webapp5.webapp5.repositories.PublisherRepository;
 import org.springframework.context.ApplicationListener;
 import org.springframework.context.event.ContextRefreshedEvent;
 import org.springframework.stereotype.Component;
@@ -13,10 +15,12 @@ public class DevBootstrap implements ApplicationListener<ContextRefreshedEvent> 
 
     private AuthorRepository authorRepository;
     private BookRepository bookRepository;
+    private PublisherRepository publisherRepository;
 
-    public DevBootstrap(AuthorRepository authorRepository, BookRepository bookRepository) {
+    public DevBootstrap(AuthorRepository authorRepository, BookRepository bookRepository, PublisherRepository publisherRepository) {
         this.authorRepository = authorRepository;
         this.bookRepository = bookRepository;
+        this.publisherRepository = publisherRepository;
     }
 
     @Override
@@ -26,9 +30,12 @@ public class DevBootstrap implements ApplicationListener<ContextRefreshedEvent> 
 
     private void initData(){
 
+        Publisher pub = new Publisher("Azamat", "Almaty");
+        publisherRepository.save(pub);
+
         //Eric
         Author nura = new Author("Nurasyl", "Dizim");
-        Book dd = new Book("The path of Abay", "1234", "Almaty Times");
+        Book dd = new Book("The path of Abay", "1234", pub);
         nura.getBooks().add(dd);
         dd.getAuthors().add(nura);
 
@@ -37,8 +44,11 @@ public class DevBootstrap implements ApplicationListener<ContextRefreshedEvent> 
 
 
         //
+        Publisher pub2 = new Publisher("Abil", "Astana");
+        publisherRepository.save(pub2);
+
         Author dima = new Author("Dima", "Uncle");
-        Book aa = new Book("Stories", "4321", "Kazakh Times");
+        Book aa = new Book("Stories", "4321", pub2);
         dima.getBooks().add(aa);
         aa.getAuthors().add(dima);
 
---
+++ src/main/java/com/webapp5/webapp5/repositories/PublisherRepository.java
@@ -0,0 +1,7 @@
+package com.webapp5.webapp5.repositories;
+
+import com.webapp5.webapp5.model.Publisher;
+import org.springframework.data.repository.CrudRepository;
+
+public interface PublisherRepository extends CrudRepository<Publisher, Long> {
+}
