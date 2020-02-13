050f3709c2c01d2bcd97d64a03587cb314bb1179
Nurasyl <n.dizimov@p-s.kz>
2020-02-13 16:06:03 +0600

    Publisher

--- src/main/java/com/webapp5/webapp5/model/Book.java
+++ src/main/java/com/webapp5/webapp5/model/Book.java
@@ -12,24 +12,25 @@ public class Book {
     private Long id;
     private String title;
     private String isbn;
-    private String publisher;
+    private Publisher publisher;
 
     @ManyToMany
     @JoinTable(name = "author_book", joinColumns = @JoinColumn(name = "book_id"),
     inverseJoinColumns = @JoinColumn(name = "author_id"))
     private Set<Author> authors = new HashSet<>();
 
+
     public Book(){
     }
 
-    public Book(String title, String isbn, String publisher, Set<Author> authors){
+    public Book(String title, String isbn, Set<Author> authors, Publisher publisher){
         this.title = title;
         this.isbn = isbn;
-        this.publisher = publisher;
         this.authors = authors;
+        this.publisher = publisher;
     }
 
-    public Book(String title, String isbn, String publisher) {
+    public Book(String title, String isbn, Publisher publisher) {
         this.title = title;
         this.isbn = isbn;
         this.publisher = publisher;
@@ -59,20 +60,20 @@ public class Book {
         this.isbn = isbn;
     }
 
-    public String getPublisher() {
-        return publisher;
-    }
-
-    public void setPublisher(String publisher) {
-        this.publisher = publisher;
-    }
-
     public Set<Author> getAuthors() {
         return authors;
     }
 
     public void setAuthors(Set<Author> authors) {
         this.authors = authors;
+    }
+
+    public Publisher getPublishers() {
+        return publisher;
+    }
+
+    public void setPublisher(Publisher publisher) {
+        this.publisher = publisher;
     }
 
     @Override
@@ -95,7 +96,6 @@ public class Book {
                 "id=" + id +
                 ", title='" + title + '\'' +
                 ", isbn='" + isbn + '\'' +
-                ", publisher='" + publisher + '\'' +
                 ", authors=" + authors +
                 '}';
     }
